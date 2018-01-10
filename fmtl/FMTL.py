8e93a7dd5b486d459172fe512b03fe78bc3e9e07
Charles-Emmanuel Dias <Charles-Emmanuel.Dias@etu.upmc.fr>
2018-01-10 16:06:47 +0100

    removed dependencies, removed iterators which are now simple idxs lists.

--- fmtl/FMTL.py
+++ fmtl/FMTL.py
@@ -1,38 +1,6 @@
 from collections import Counter
-from tqdm import tqdm
 import itertools
 
-
-class FMTL_iterator():
-    """
-    Simple indexed iterator on FMTL
-    """
-    def __init__(self,fmtl,idxs):
-        super(FMTL_iterator, self).__init__()
-        self.fmtl = fmtl
-        self.idxs = idxs
-        
-    def __getitem__(self,i):
-        return self.fmtl[i]
-    
-    def __len__(self):
-        return len(self.idxs)
-
-    def __iter__(self):
-        self.iter_idx = self.idxs.__iter__()
-        return self
-    
-    def __next__(self):
-        idx = self.iter_idx.__next__()
-        return self.fmtl[idx]
-
-    def prebuild(self):
-        return [x for x in tqdm(self,desc="Prebuilding")]
-        
-    @staticmethod
-    def from_seq(fmtl,idxs):
-        return tuple([FMTL_iterator(fmtl,idx) for idx in idxs])
-        
 
 class FMTL():
     
@@ -62,12 +30,26 @@ class FMTL():
             return self.tuplelist[index]
         else:
             t = list(self.tuplelist[index])
+
             for i,m in self.mappings.items():
-                t[i] = self._rec_apply(m,t[i],self.unknown.get(i))
+                if type(m) is dict:
+                    t[i] = self._rec_apply(m.__getitem__,t[i],self.unknown.get(i))
+                else:
+                    try:
+                        t[i] = m(t[i])
+                    except Exception:
+                        if self.unknown.get(i) is not None:
+                            return self.unknown.get(i)
+                        else:
+                            raise KeyError("No mapping or placeholder for value: {}".format(i))
 
             return tuple(t)
 
     def __iter__(self):
+        self.iter_idxs = range(len(self.tuplelist)).__iter__()
+        return self
+    
+    def indexed_iter(self,idxs):
         self.iter_idxs = range(len(self.tuplelist)).__iter__()
         return self
 
@@ -86,11 +68,6 @@ class FMTL():
 
         raise IndexError("field {} doesn't exist".format(field))
     
-    def _dic_or_fun(self,mapper):
-        if type(mapper) is dict:
-            return mapper.__getitem__
-        else:
-            return mapper
 
     def _rec_apply(self,f,item,unk=None):
         if isinstance(item,list) or isinstance(item,tuple):
@@ -98,7 +75,7 @@ class FMTL():
         else:
             try:
                 return f(item)
-            except:
+            except Exception:
                 if unk is not None:
                     return unk
                 else:
@@ -110,7 +87,7 @@ class FMTL():
         Sets a mapping for a tuple field. Mappings are functions of a field value or dict
         """
         field = self._f2i(field)
-        self.mappings[field] = self._dic_or_fun(mapping)
+        self.mappings[field] = mapping
 
         if unk is not None:
             self.unknown[field] = unk
@@ -118,13 +95,13 @@ class FMTL():
         return mapping
 
     def field_gen(self, field, key_iter=None):
+
         field = self._f2i(field)
-        if key_iter is not None:
-            for x in key_iter:
-                yield x[field]
-        else:
-            for x in self:
-                yield x[field]
+        if key_iter is None:
+            key_iter = range(len(self))
+
+        for idx in key_iter:
+            yield self[idx][field]
 
     def get_stats(self, field, key_iter=None, verbose=False):
         """
@@ -159,49 +136,3 @@ class FMTL():
     
         d2k = {c:i for i,c in enumerate(d_keys,offset)}
         return d2k
-
-    def prebuild(self):
-        """
-        Freeze mappings into a new FMTL instance.
-        """
-        return FMTL([x for x in tqdm(self,desc="Prebuilding")],self.rows)
-
-    @staticmethod
-    def from_list(datatuples,splits,split_num=0,validation=0.5,rows=None):
-        """
-        Builds train/val/test indexes sets from tuple list and split list
-        Validation set at 0.5 if n split is 5 gives an 80:10:10 split as usually used.
-        """
-        train,test = [],[]
-
-        for idx,split in tqdm(enumerate(splits),total=len(splits),desc="Building train/test of split #{}".format(split_num)):
-            if split == split_num:
-                test.append(idx)
-            else:
-                train.append(idx)
-
-        if len(test) <= 0:
-                raise IndexError("Test set is empty - split {} probably doesn't exist".format(split_num))
-
-        if rows and type(rows) is tuple:
-            rows = {v:k for k,v in enumerate(rows)}
-            print("Tuples rows are the following:")
-            print(rows)
-
-        if validation > 0:
-
-            if 0 < validation < 1:
-                val_len = int(validation * len(test))
-
-            validation = test[-val_len:]
-            test = test[:-val_len]
-
-        else:
-            validation = []
-
-        idxs = [train,test,validation]
-        fmtl = FMTL(datatuples,rows)
-        iters = FMTL_iterator.from_seq(fmtl,idxs)
-
-        return (fmtl,iters)
-
